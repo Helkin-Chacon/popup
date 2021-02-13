44d156b0e3cd75d68559e7caa1b516a959c39e55
Helkin-Chacon <63528430+Helkin-Chacon@users.noreply.github.com>
2021-02-13 10:28:46 -0500

    popup with icons

---
+++ PopDink2/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using PopDink2.views;
+namespace PopDink2
+{
+	public partial class App : Application
+	{
+		public App()
+		{
+			InitializeComponent();
+
+			MainPage = new PopDink();
+		}
+
+		protected override void OnStart()
+		{
+		}
+
+		protected override void OnSleep()
+		{
+		}
+
+		protected override void OnResume()
+		{
+		}
+	}
+}
---
+++ PopDink2/viewModel/viewModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace PopDink2.viewModel
+{
+    class viewModel : INotifyPropertyChanged
+    {
+        private bool displayPopup;
+
+        public ICommand OpenPopupCommand { get; set; }
+
+        public bool DisplayPopup
+        {
+            get
+            {
+                return displayPopup;
+            }
+            set
+            {
+                displayPopup = value;
+                RaisePropertyChanged("DisplayPopup");
+            }
+        }
+
+        public viewModel()
+        {
+            OpenPopupCommand = new Command(OpenPopup);
+        }
+
+        private void OpenPopup()
+        {
+            DisplayPopup = true;
+        }
+
+        #region INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void RaisePropertyChanged(string propertyName)
+        {
+            if (this.PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
+
+    #endregion
